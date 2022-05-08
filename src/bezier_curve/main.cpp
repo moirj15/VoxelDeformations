bd0413fb3775793b2da467100c80dfd102658c37
moirj15 <moirj15@gmail.com>
2022-05-08 16:52:14 -0400

    misc work

--- src/bezier_curve/main.cpp
+++ src/bezier_curve/main.cpp
@@ -1,27 +1,11 @@
 #include "glm/gtx/compatibility.hpp"
-#include "utils.h"
+#include <utils.h>
 
 #include <SDL2/SDL.h>
 #include <focus.hpp>
 #include <glm/vec2.hpp>
 #include <memory>
 #include <optional>
-namespace utils
-{
-glm::vec2 ScreenSpaceToNDC(const glm::vec2 &mouse_pos, const int screen_width, const int screen_height)
-{
-    return {(mouse_pos.x - (static_cast<f32>(screen_width) / 2.0f)) / (static_cast<f32>(screen_width) / 2.0f),
-        ((static_cast<f32>(screen_height) - mouse_pos.y) - (static_cast<f32>(screen_height) / 2.0f))
-            / (static_cast<f32>(screen_height) / 2.0f)};
-}
-
-glm::ivec2 NDCToScreenSpace(const glm::vec2 &screen_space, const int screen_width, const int screen_height)
-{
-    return {(screen_space.x * (static_cast<f32>(screen_width) / 2.0f)) + (static_cast<f32>(screen_width) / 2.0f),
-        (-screen_space.y * (static_cast<f32>(screen_height) / 2.0f)) + (static_cast<f32>(screen_height) / 2.0f)};
-}
-
-} // namespace utils
 
 struct DataManager {
     static constexpr f32 point_size = 10.0f;
